fa73dad13c3503230dd3b132b6f0eedec50a9e50
unknown <benorule@outlook.com>
2020-10-21 00:28:58 +1030

    configure middleware needed for authentication in server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// const passport = require("passport");
+const passport = require("./config/passport");
 
-// const users = require("./routes/api/users");
 const goals = require("./routes/api/goals");
 
 const app = express();
@@ -11,19 +9,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// body-parser middleware to handle HTTP POST requests 
-// app.use(
-//     bodyParser.urlencoded({
-//         extended: false
-//     })
-// );
-// app.use(bodyParser.json());
-
 // configure database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/goal", { useNewUrlParser: true });
 
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 // routes
-// app.use("/api/users", users);
 app.use(goals);
 
 const port = process.env.PORT || 3001;
