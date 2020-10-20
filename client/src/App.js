274cc1d881a66e5197723f1cde6a9d5c30e885eb
unknown <benorule@outlook.com>
2020-10-20 18:51:31 +1030

    solve functionality issue by moving functions out of app into track component

--- client/src/App.js
+++ client/src/App.js
@@ -1,107 +1,18 @@
-import React, { useState, useEffect } from "react";
-import GoalInfo from "./components/Goal";
-import MonthBtns from "./components/MonthBtns";
+import React from "react";
 import Nav from "./components/Nav";
-import GoalContext from "./utils/GoalContext";
-import Form from "./components/Form";
 import "./App.css";
-import axios from "axios";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Track from "./pages/track.js";
 
 function App() {
-  const [goalState, setGoalState] = useState({
-    value: "deposit",
-    savings: 0,
-    timeframe: 0,
-    deposit: 0,
-    month: true
-  });
-
-  function changeMonth(Month) {
-    if (Month === "determined") {
-      goalState.savings -= 100;
-      goalState.timeframe -= 1;
-      checkGoal();
-    }
-    else {
-    }
-    setGoalState({
-      ...goalState,
-      Month
-    });
-  }
-
-  function checkGoal() {
-    if (goalState.timeframe === 0) {
-      alert("You have accomplished your goal!")
-    }
-    else {
-      // setNewGoal();
-    }
-  }
-
-  const calculate = (event) => {
-    event.preventDefault();
-    if (goalState.value === "deposit") {
-      // get user input for savings and time frame
-      setGoalState({ ...goalState, deposit: goalState.savings / goalState.timeframe });
-    }
-    else if (goalState.value === "timeframe") {
-      // get user input for savings and deposit
-      setGoalState({ ...goalState, timeframe: goalState.savings / goalState.deposit });
-    }
-    else {
-      // get user input for savings and deposit
-      setGoalState({ ...goalState, savings: goalState.timeframe * goalState.deposit });
-    }
-  }
-
-  const save = () => {
-    axios.post("/api/goal/save", {
-      savings: goalState.savings,
-      timeframe: goalState.timeframe,
-      deposit: goalState.deposit
-    }).then((res) => {
-      console.log(res);
-    });
-  }
-
-  const update = () => {
-    axios.put("/api/goal/update", {
-      savings: goalState.savings,
-      timeframe: goalState.timeframe,
-      deposit: goalState.deposit,
-      _id: goalState._id
-    }).then((res) => {
-      console.log(res);
-    });
-  }
-
-  useEffect(() => {
-    axios.get("/api/goal").then((res) => {
-      setGoalState({ ...goalState, ...res.data[0] })
-    });
-  }, []);
-
   return (
     <Router>
       <div>
         <Nav />
-        <Route exact path="/track" component={Track} />
+        <Route exact path="/" component={Track} />
       </div>
     </Router>
   );
 }
-
-//   return (
-//     <GoalContext.Provider value={{ goalState, setGoalState, calculate }}>
-//       <Nav />
-//       <GoalInfo />
-//       <MonthBtns changeMonth={changeMonth} save={save} goalState={goalState} update={update} />
-//       <Form />
-//     </GoalContext.Provider>
-//   );
-// }
 
 export default App;
--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,25 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GoalInfo from "../components/Goal";
 import MonthBtns from "../components/MonthBtns";
-import Nav from "../components/Nav";
 import GoalContext from "../utils/GoalContext";
 import Form from "../components/Form";
 import "../App.css";
-import goalState from "../App.js";
-import setGoalState from "../App.js";
-import calculate from "../App.js";
-import changeMonth from "../App.js";
-import save from "../App.js";
-import update from "../App.js";
+import axios from "axios";
+
 
 function Track() {
-return (
-    <GoalContext.Provider value={{ goalState, setGoalState, calculate }}>
-      <GoalInfo />
-      <MonthBtns changeMonth={changeMonth} save={save} goalState={goalState} update={update} />
-      <Form />
-    </GoalContext.Provider>
-  );
+    const [goalState, setGoalState] = useState({
+        value: "deposit",
+        savings: 0,
+        timeframe: 0,
+        deposit: 0,
+        month: true
+    });
+
+    function changeMonth(Month) {
+        if (Month === "determined") {
+            goalState.savings -= 100;
+            goalState.timeframe -= 1;
+            checkGoal();
+        }
+        else {
+        }
+        setGoalState({
+            ...goalState,
+            Month
+        });
+    }
+
+    function checkGoal() {
+        if (goalState.timeframe === 0) {
+            alert("You have accomplished your goal!")
+        }
+        else {
+            // setNewGoal();
+        }
+    }
+
+    const calculate = (event) => {
+        event.preventDefault();
+        if (goalState.value === "deposit") {
+            // get user input for savings and time frame
+            setGoalState({ ...goalState, deposit: goalState.savings / goalState.timeframe });
+        }
+        else if (goalState.value === "timeframe") {
+            // get user input for savings and deposit
+            setGoalState({ ...goalState, timeframe: goalState.savings / goalState.deposit });
+        }
+        else {
+            // get user input for savings and deposit
+            setGoalState({ ...goalState, savings: goalState.timeframe * goalState.deposit });
+        }
+    }
+
+    const save = () => {
+        axios.post("/api/goal/save", {
+            savings: goalState.savings,
+            timeframe: goalState.timeframe,
+            deposit: goalState.deposit
+        }).then((res) => {
+            console.log(res);
+        });
+    }
+
+    const update = () => {
+        axios.put("/api/goal/update", {
+            savings: goalState.savings,
+            timeframe: goalState.timeframe,
+            deposit: goalState.deposit,
+            _id: goalState._id
+        }).then((res) => {
+            console.log(res);
+        });
+    }
+
+    useEffect(() => {
+        axios.get("/api/goal").then((res) => {
+            setGoalState({ ...goalState, ...res.data[0] })
+        });
+    }, []);
+
+    return (
+        <GoalContext.Provider value={{ goalState, setGoalState, calculate }}>
+            <GoalInfo />
+            <MonthBtns changeMonth={changeMonth} save={save} goalState={goalState} update={update} />
+            <Form />
+        </GoalContext.Provider>
+    );
 }
 
 export default Track;
